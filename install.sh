1951bfe261bda5bdeabbb22d596c89958cb043c8
AverageUser125 <naorvan1@gmail.com>
2022-05-28 16:43:45 +0300

    Finished install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
 # Updating System
 dnf update -y
 
 # Making .config and Moving dotfiles and Background to .config
 mkdir ~/.config
+chown $(whoami): ~/.config
 mv ./dotconfig/* ~/.config
 mv ./bg.jpg ~/.config
 
 # Installing Essential Programs 
-dnf install sddm bspwm sxhkd kitty rofi polybar picom thunar
-# Other less important Programs
-dnf install mangohud gimp vim
+dnf install sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit
+# Installing Other less important Programs
+dnf install mangohud gimp vim lxappearance
+# Installing Custom ocs-url package
+dnf install ./rpm-packages/ocs-url-3.1.0-1.fc20.x86_64.rpm
 
-# Installing Meslo and FiraCode fonts
-
+# Installing fonts
+dnf install fontawesome-fonts fontawesome-fonts-web
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
 
 # Enabling Services and Graphical User Interface
 systemctl enable sddm
