8887d83a761c5b580d981efc6f6528dac4635284
Raul Rodrigues <raulferrodrigues@gmail.com>
2022-07-19 08:16:01 -0300

    changed the way it does sudo

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
 # Updating System
-dnf update -y
+sudo dnf update -y
 
 # Making .config and Moving dotfiles and Background to .config
 mkdir ~/.config
@@ -16,14 +10,14 @@ mv ./dotconfig/* ~/.config
 mv ./bg.jpg ~/.config
 
 # Installing Essential Programs 
-dnf install -y sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit
+sudo dnf install -y sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit
 # Installing Other less important Programs
-dnf install -y mangohud gimp vim lxappearance
+sudo dnf install -y mangohud gimp vim lxappearance
 # Installing Custom ocs-url package
-dnf install -y ./rpm-packages/ocs-url-3.1.0-1.fc20.x86_64.rpm
+sudo dnf install -y ./rpm-packages/ocs-url-3.1.0-1.fc20.x86_64.rpm
 
 # Installing fonts
-dnf install -y fontawesome-fonts fontawesome-fonts-web
+sudo dnf install -y fontawesome-fonts fontawesome-fonts-web
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
@@ -34,5 +28,5 @@ fc-cache -vf
 rm ./FiraCode.zip ./Meslo.zip
 
 # Enabling Services and Graphical User Interface
-systemctl enable sddm
-systemctl set-default graphical.target
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
